fe2ae8414f8bbcdd599903cf668ace22b72bef03
kanthi Subramanian <subkanthi@gmail.com>
2023-10-12 16:53:57 -0400

    switched to use bitspoke mysql operator(Oracle operator throws error with not able to find privsecrets).

--- create-stack-mysql-etl.sh
+++ create-stack-mysql-etl.sh
@@ -50,6 +50,8 @@ done
 #argocd app sync mysql-operator-crds --revision add_sink_connector
 #argocd app create mysql-operator-crds --repo https://github.com/Altinity/argocd-examples-clickhouse.git --path apps/mysql-operator-crds --dest-server https://kubernetes.default.svc --dest-namespace ch --revision add_sink_connector
 
+argocd app create mysql --repo https://github.com/Altinity/argocd-examples-clickhouse.git --path apps/mysql --dest-server https://kubernetes.default.svc --dest-namespace ch --revision add_sink_connector
+
 
 #argocd app create prometheus-operator-crds \
 # --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
